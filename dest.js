5974c99ee5bba24fe78c0fbc32aac451ae216fee
PatWil <patrick.williams@tlgcohort.com>
2021-04-17 07:49:18 -0700

    if then  default image

--- dest.js
+++ dest.js
@@ -15,27 +15,31 @@ function handleSum(evt) {
   //   alert(evt.target.name.value);
   const destination = evt.target.dest.value;
   const location = evt.target.Location.value;
-  let photo = evt.target.Photo.value;
+  let oto = evt.target.Photo.value;
 
   const description = evt.target.Description.value;
 
-  var img = document.createElement("img");
+  let img = document.createElement("img");
   img.src =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfEjaLquQ38Bmm-2W_Pgu1q6PXB7sC32impg&usqp=CAU";
-  var man = `<img src="${img.src}">`;
+  let man = `<img src="${img.src}">`;
 
   photo = document.createElement("img");
   photo.src = evt.target.Photo.value;
-  var man2 = `<img src="${photo.src}">`;
+  let man2 = `<img src="${photo.src}">`;
+
+  let pic;
 
   const contains = document.getElementById("toadd");
   const myDiv = document.createElement("div");
-  // if (photo.length === 0) {
-  //   photo = man;
-  // }
+  if (oto.length === 0) {
+    pic = man;
+  } else {
+    pic = man2;
+  }
   myDiv.innerHTML = `
   <div>
-  ${man2}
+  ${pic}
   <p>you chose ${destination} - located in ${location}</p>
   <p>${description}</p>
   <button>edit</button><span><button>remove</button>
