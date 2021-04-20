ab23a4d9e40023e9e0af477d17ebe2e3ee944178
PatWil <patrick.williams@tlgcohort.com>
2021-04-20 10:32:16 -0700

    api stufff

--- dest.js
+++ dest.js
@@ -1,5 +1,56 @@
+const contains = document.getElementById("toadd");
+
 //add event listner to form on submit
 document.getElementById("info").addEventListener("submit", handleSum);
+
+function getApi() {
+  let destination = document.getElementById("dest").value;
+  let API = `https://api.unsplash.com/search/photos?client_id=O1CcoG842k4QrdW3z9oSbnxL9wOPaMOx0LXopCuZ8us&query=${destination}`;
+  // console.log(API);
+  fetch(API)
+    .then((resp) => resp.json())
+    .then((data) => display(data));
+}
+function display(data) {
+  //from handle function
+  let destination = document.getElementById("dest").value;
+  //take dest and sent to get api function
+  let location = document.getElementById("Location").innerHTML;
+  // let oto = document.getElementById("Photo").innerHTML;
+  let description = document.getElementById("Description").innerHTML;
+  // console.log(data.results);
+  let unsplash = data.results[0].urls.small;
+
+  // unsplash = `https://api.unsplash.com/search/photos?client_id=O1CcoG842k4QrdW3z9oSbnxL9wOPaMOx0LXopCuZ8us&query=${destination}`;
+
+  ///default img
+  // <img src="" class="newest" />
+  let img = document.createElement("img"); // <img />
+  // img.src =
+  //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfEjaLquQ38Bmm-2W_Pgu1q6PXB7sC32impg&usqp=CAU";
+  img.src =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR11Te4EqTBLfu_84WL9o_mJ7ZPlXyxRmsQAg&usqp=CAU";
+  let man = `<img id="defualt" src="${img.src}" class="img-thumbnail">`;
+
+  //given image
+  contains.innerHTML = "";
+  /// html element to be displayed
+  contains.innerHTML += `<div id="outer" >
+  <img src=${unsplash}>
+  <hr>
+  <div class="inner height">
+  <p id="dandl">you chose ${destination} - located in ${location}</p>
+  </div>
+  <div class="text-muted daTop">
+  <p id="des">Description: ${description}</p>
+  </div>
+  <hr>
+  <div class="inner">
+  <button id="edit" class="btn btn-warning">edit</button><span><button id="remove" class="btn btn-danger">remove</button>
+  </div>
+  </div>
+  `;
+}
 
 //delete card
 function remDel() {
@@ -27,68 +78,5 @@ function editCard() {
 //sumbt event to display card
 function handleSum(evt) {
   evt.preventDefault();
-
-  let destination = evt.target.dest.value;
-  let location = evt.target.Location.value;
-  let oto = evt.target.Photo.value;
-
-  let description = evt.target.Description.value;
-
-  ///default img
-  // <img src="" class="newest" />
-  let img = document.createElement("img"); // <img />
-  // img.src =
-  //   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfEjaLquQ38Bmm-2W_Pgu1q6PXB7sC32impg&usqp=CAU";
-  img.src =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR11Te4EqTBLfu_84WL9o_mJ7ZPlXyxRmsQAg&usqp=CAU";
-  let man = `<img id="defualt" src="${img.src}" class="img-thumbnail">`;
-
-  // //ask nelly
-  // img.setAttribute("id", "defualt");
-  // img.setAttribute("class", "img-thumbnail");
-
-  //given image
-  let photo = document.createElement("img");
-  photo.src = evt.target.Photo.value;
-  let man2 = `<img id="given" src="${photo.src}" class="img-thumbnail">`;
-
-  //ask nelly
-  // photo.setAttribute("id", "given");
-  // photo.setAttribute("class", "img-thumbnail");
-
-  //logic for default img if empty text
-  let pic;
-  if (oto.length === 0) {
-    pic = man;
-  } else {
-    pic = man2;
-  }
-  ///grab target area to insert assign to a variable and create new empty html element
-  const contains = document.getElementById("toadd");
-  const myDiv = document.createElement("div");
-
-  //assigning empty html element to html div with input values inserted
-  myDiv.innerHTML = `
-  <div id="outer" >
-  ${pic}
-  <hr>
-  <div class="inner height">
-  <p id="dandl">you chose ${destination} - located in ${location}</p>
-  </div>
-  <div class="text-muted daTop">
-  <p id="des">Description: ${description}</p>
-  </div>
-  <hr>
-  <div class="inner">
-  <button id="edit" class="btn btn-warning">edit</button><span><button id="remove" class="btn btn-danger">remove</button>
-  </div>
-  </div>
-  `;
-
-  //append child{html element}
-  contains.appendChild(myDiv);
-
-  //add listener to html elements buttons on click
-  document.getElementById("edit").addEventListener("click", editCard);
-  document.getElementById("remove").addEventListener("click", remDel);
+  getApi();
 }
