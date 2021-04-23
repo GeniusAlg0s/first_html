3501c774d99fb4bf0a9e630a183eeb23e0d5f8f9
PatWil <patrick.williams@tlgcohort.com>
2021-04-23 13:14:49 -0700

    display card onload

--- dest.js
+++ dest.js
@@ -1,5 +1,61 @@
 const contains = document.getElementById("toadd");
+const herokuAPI = `https://destapi.herokuapp.com/`;
 
+/// when data loads display that data
+fetch(herokuAPI)
+  .then((res) => res.json())
+  .then((data) => displayDest(data));
+
+//displayDest funct
+function displayDest(data) {
+  for (let i = 0; i < data.length; i++) {
+    let name = data[i].name;
+    let location = data[i].location;
+    let description = data[i].description;
+    let nphoto = data[i].photo;
+    console.log(data[i].name);
+
+    let nameElement = document.createElement("h1");
+    let locationElement = document.createElement("p");
+    let descriptionElement = document.createElement("p");
+    let nphotoElement = document.createElement("img");
+    let staticContainer = document.createElement("div");
+    let innerOneContainer = document.createElement("div");
+    let innertwoContainer = document.createElement("div");
+    let innerthreeContainer = document.createElement("div");
+
+    let edit = document.createElement("button");
+    edit.setAttribute("class", "btn btn-warning");
+    edit.innerHTML = "edit";
+
+    let remove = document.createElement("button");
+    remove.setAttribute("class", "btn btn-danger");
+    remove.innerHTML = "remove";
+
+    innerOneContainer.setAttribute("class", "inner height");
+    staticContainer.setAttribute("id", "outer");
+    nphotoElement.setAttribute("src", nphoto);
+
+    nameElement.innerHTML = name;
+    locationElement.innerHTML = location;
+    descriptionElement.innerHTML = description;
+
+    innerOneContainer.appendChild(nameElement);
+    innerOneContainer.appendChild(locationElement);
+
+    innertwoContainer.appendChild(descriptionElement);
+
+    innerthreeContainer.appendChild(edit);
+    innerthreeContainer.appendChild(remove);
+
+    staticContainer.appendChild(nphotoElement);
+    staticContainer.appendChild(innerOneContainer);
+    staticContainer.appendChild(innertwoContainer);
+    staticContainer.appendChild(innerthreeContainer);
+
+    contains.appendChild(staticContainer);
+  }
+}
 //add event listner to form on submit
 document.getElementById("info").addEventListener("submit", handleSum);
 
@@ -9,7 +65,7 @@ function getApi() {
     destination = "cat";
   }
   let API = `https://api.unsplash.com/search/photos?client_id=O1CcoG842k4QrdW3z9oSbnxL9wOPaMOx0LXopCuZ8us&query=${destination}`;
-  
+
   fetch(API)
     .then((resp) => resp.json())
     .then((data) => display(data));
@@ -19,14 +75,14 @@ function display(data) {
   let destination = document.getElementById("dest").value;
   //take dest and sent to get api function
   let location = document.getElementById("Location").innerHTML;
-  
+
   let description = document.getElementById("Description").innerHTML;
-  
+
   let unsplash = data.results[0].urls.small;
   ///default img
 
   let img = document.createElement("img"); // <img />
-  
+
   img.src =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR11Te4EqTBLfu_84WL9o_mJ7ZPlXyxRmsQAg&usqp=CAU";
   let man = `<img id="defualt" src="${img.src}" class="img-thumbnail">`;
